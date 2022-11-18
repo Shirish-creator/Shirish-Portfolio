86446328c9a38f4e9e0450e82fc5d4517d3276f3
Shirish-creator <84584836+Shirish-creator@users.noreply.github.com>
2022-11-18 16:11:23 +0545

    append child source

--- src/uploader.js
+++ src/uploader.js
@@ -2,30 +2,33 @@ import horse from "./assets/videos/horsenft.mp4"
 import porsche from "./assets/videos/porsche.mp4"
 
 let videos=document.querySelectorAll('.threeDvideo')
-var source = document.querySelectorAll(".videosource");
+// var source = document.querySelectorAll(".videosource");
 
-console.log(source)
+// console.log(source)
 
 for(let n=0;n<2;n++){
     let VideoID=videos[n].id;
+
+    let Source=document.createElement("source")
+    videos[n].appendChild(Source);
     
     if(VideoID==="nft"){
         
-        source[n].setAttribute('src', horse);
-        source[n].setAttribute('type', 'video/mp4');
-        console.log(source[n].setAttribute,"horse")
+        Source.setAttribute('src', horse);
+        Source.setAttribute('type', 'video/mp4');
+        console.log(Source.setAttribute,"horse")
         
 
 
     }
     if(VideoID==="porsche"){
-        source[n].setAttribute('src', porsche);
-        source[n].setAttribute('type', 'video/mp4'); 
-        console.log(source[n].setAttribute,"porsche")
+        Source.setAttribute('src', porsche);
+        Source.setAttribute('type', 'video/mp4'); 
+        console.log(Source.setAttribute,"porsche")
         
 
     }
-    videos[n].appendChild(source[n]);
+    
 }
 
 

11ae4040ef0785f92eebe006a5b64de52160b171
Shirish-creator <84584836+Shirish-creator@users.noreply.github.com>
2022-11-18 14:17:03 +0545

    video test 5

--- src/uploader.js
+++ src/uploader.js
@@ -1,29 +1,29 @@
-// import horse from "./assets/videos/horsenft.mp4"
+import horse from "./assets/videos/horsenft.mp4"
 
 
-// var video = document.getElementById('thevideo');
-// var source = document.querySelector(".videhorse");
+var video = document.getElementById('thevideo');
+var source = document.querySelector(".videhorse");
 
-// source.setAttribute('src', horse);
-// source.setAttribute('type', 'video/mp4');
+source.setAttribute('src', horse);
+source.setAttribute('type', 'video/mp4');
 
-// video.appendChild(source);
-// video.play();
-// console.log({
-//   src: source.getAttribute('src'),
-//   type: source.getAttribute('type'),
-// });
+video.appendChild(source);
+video.play();
+console.log({
+  src: source.getAttribute('src'),
+  type: source.getAttribute('type'),
+});
 
-// setTimeout(function() {
-//   video.pause();
+setTimeout(function() {
+  video.pause();
 
-//   source.setAttribute('src', horse);
-//   source.setAttribute('type', 'video/webm');
+  source.setAttribute('src', horse);
+  source.setAttribute('type', 'video/webm');
 
-//   video.load();
-//   video.play();
-//   console.log({
-//     src: source.getAttribute('src'),
-//     type: source.getAttribute('type'),
-//   });
-// }, 3000);
+  video.load();
+  video.play();
+  console.log({
+    src: source.getAttribute('src'),
+    type: source.getAttribute('type'),
+  });
+}, 3000);

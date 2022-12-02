2a85388667a2cc508d42511517c2fc14bb77a032
Shirish-creator <84584836+Shirish-creator@users.noreply.github.com>
2022-12-02 12:45:47 +0545

    loader fixed

--- src/loader.js
+++ src/loader.js
@@ -1,27 +1,27 @@
-var loader=document.querySelector('.loader')
+// var loader=document.querySelector('.loader')
 
-window.addEventListener('load',()=>{
-    loader.classList.add('loaded')
-})
+// window.addEventListener('load',()=>{
+//     loader.classList.add('loaded')
+// })
 
 
 
-// function display_c(){
-// var refresh=1000; // Refresh rate in milli seconds
-// mytime=setTimeout('display_ct()',refresh)
+// // function display_c(){
+// // var refresh=1000; // Refresh rate in milli seconds
+// // mytime=setTimeout('display_ct()',refresh)
 
-// }
+// // }
 
-// function display_ct() {
+// // function display_ct() {
 
-// var d = new Date(); 
-// var H=d.getHours(); 
-// var M=d.getMinutes(); 
-// var S= d.getSeconds();    
-// var D=d.getFullYear()
+// // var d = new Date(); 
+// // var H=d.getHours(); 
+// // var M=d.getMinutes(); 
+// // var S= d.getSeconds();    
+// // var D=d.getFullYear()
 
-// document.querySelector('.time').innerHTML = H + ":"+ M +":" + S + " " + "(Local Time)";
-// document.querySelector('.dated').innerHTML=D;
-// display_c();
-// }
+// // document.querySelector('.time').innerHTML = H + ":"+ M +":" + S + " " + "(Local Time)";
+// // document.querySelector('.dated').innerHTML=D;
+// // display_c();
+// // }
 

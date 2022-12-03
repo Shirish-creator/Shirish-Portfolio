4d1da22cc5dba0d6e1025c0da5e180f2d69c9826
Shirish-creator <84584836+Shirish-creator@users.noreply.github.com>
2022-12-03 12:10:39 +0545

    preloader added

--- src/loader.js
+++ src/loader.js
@@ -25,3 +25,54 @@
 // // display_c();
 // // }
 
+import gsap from 'gsap';
+
+        const tll = gsap.timeline({
+            paused: "true"
+        });
+        tll.to("#percent, #bar",{
+            duration:.2,
+            opacity: 0,
+            zIndex: -1
+        });
+        tll.to("#preloader",{
+            duration: .8,
+            width: "0%",
+        });
+        tll.to("#loadhead",{
+            duration: 0,
+            marginLeft:"-100px"
+        });
+        // tll.from(".container",{
+        //     duration: 1.5,
+        //     y: "-150%"
+        // },"-=.2");
+        // tll.to(".container h1",{
+        //     opacity:1,
+        //     textShadow: "12px 20px rgba(255,255,255,0.23)",
+        //     skewY: 10,
+        //     y: "10%",
+        //     stagger: {
+        //         amount: .4
+        //     }
+        // });
+        var width = 1;
+        var bar = document.getElementById("barconfrm");
+        var id;
+        function move(){
+            id = setInterval(frame,10);
+
+        }
+        function frame(){
+            if(width>=100){
+                clearInterval(id);
+                tll.play();
+            }
+            else{
+                width++;
+                bar.style.width = width + "%";
+                document.getElementById("percent").innerHTML = width + "%";
+            }
+        }
+
+        window.addEventListener('load',move())
